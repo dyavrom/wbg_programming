86632e2673a23dc359b80cae60dc117e8188b858
Deeba Yavrom <18711054+dyavrom@users.noreply.github.com>
2022-07-11 20:01:59 -0400

    Update random_sample_.R

--- random_sample_.R
+++ random_sample_.R
@@ -12,8 +12,8 @@ conn = dbConnect(drvr, "jdbc:oracle:thin:@//localhost:####/******",
                  user = "dyn",
                  password = "********")
 
-## run sql script to pull permitted data
-source("dtj_sql.r") ## sql not written by me. 
+## once connected to Oracle DB, run sql script to pull data to join (dtj)
+source("dtj_sql.r") 
 
 #load data
 data = read.csv("data_to_sample.csv", stringsAsFactors = FALSE, header = TRUE)
@@ -26,13 +26,12 @@ colnames(dtj) <- c('Handler_ID','Is_OnCAProgressTrack_Flag','Is_Permitted_Flag')
 data_join = left_join(data, dtj, by = "Handler_ID")
 data_join = data_join %>% relocate(Is_OnCAProgressTrack_Flag, Is_Permitted_Flag, .after = Handler_Name)
 
-#split data and select a random sample of 5% of the columns per Region
+#split data by region and select a random sample of 5% of the columns
 data_join_s = lapply(split(data_join, data_join$Region),
                 function(x) x[sample(nrow(x), nrow(x) * 0.05),])
 
 #rename data list entries
 names(data_join_s) <- c("R01_sample", "R02_sample", "R03_sample","R04_sample", "R05_sample", "R06_sample", "R07_sample", "R08_sample", "R09_sample", "R10_sample")
-
 
 ##write csv based off of large data list names
 lapply(1:length(data_join_s), function(i) write.csv(data_join_s[[i]], 
